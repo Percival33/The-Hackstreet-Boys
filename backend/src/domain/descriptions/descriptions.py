ab88cf031907a7cd473272f98973efd06fe048d6
Mikolaj Szawerda <mszawerda@box.com>
2024-09-28 19:58:12 +0200

    add additional actions

---
+++ backend/src/domain/descriptions/descriptions.py
@@ -0,0 +1,123 @@
+PCC3_DESCRIPTION = """
+## Co podlega obowiązkowi podatkowemu
+
+Obowiązek podatkowy w podatku od czynności cywilnoprawnych (PCC)
+powstaje, gdy:
+
+-   zawierasz:
+    -   umowę sprzedaży oraz zamiany rzeczy lub praw majątkowych,
+    -   umowę pożyczki pieniędzy lub rzeczy oznaczonych tylko co do
+        gatunku
+    -   umowę darowizny - w części dotyczącej przejęcia przez
+        obdarowanego długów i ciężarów albo zobowiązań darczyńcy,
+    -   umowę dożywocia,
+    -   umowę o dział spadku lub umowę o zniesienie współwłasności - w
+        części dotyczącej spłat lub dopłat,
+    -   umowę depozytu nieprawidłowego,
+    -   umowę spółki,
+-   składasz oświadczenie lub zawierasz umowę o ustanowieniu hipoteki,
+-   dokonasz ustanowienia ograniczonych praw rzeczowych w postaci
+    odpłatnej służebności i odpłatnego użytkowania, w tym
+    nieprawidłowego,
+-   uprawomocnia się orzeczenie sądu - jeżeli wywołuje takie same skutki
+    prawne jak podlegające opodatkowaniu czynności cywilnoprawne,
+-   dostajesz wyrok sądu polubownego albo dojdzie do ugody - jeżeli
+    wywołują takie same skutki prawne jak podlegające opodatkowaniu
+    czynności cywilnoprawne.
+
+Przepisy o umowie o dziale spadku oraz umowie o zniesieniu
+współwłasności stosuje się odpowiednio także do:
+
+1.  nabycia własności rzeczy wspólnej lub wspólnego prawa majątkowego
+    albo ich części przez niektórych dotychczasowych współwłaścicieli na
+    dalszą współwłasność -- w części spłat lub dopłat,
+2.  odpłatnego wyodrębnienia własności lokali na rzecz niektórych lub
+    wszystkich współwłaścicieli.
+
+W przypadku umowy spółki za zmianę umowy uważa się:
+
+1.  przy spółce osobowej -- wniesienie lub podwyższenie wkładu, którego
+    wartość powoduje zwiększenie majątku spółki, pożyczkę udzieloną
+    spółce przez wspólnika, dopłaty oraz oddanie przez wspólnika spółce
+    rzeczy lub praw majątkowych do nieodpłatnego używania;
+2.  przy spółce kapitałowej -- podwyższenie kapitału zakładowego z
+    wkładów lub ze środków spółki oraz dopłaty;
+3.  przekształcenie lub łączenie spółek, jeżeli ich wynikiem jest
+    zwiększenie majątku spółki osobowej lub podwyższenie kapitału
+    zakładowego spółki kapitałowej;
+4.  przeniesienie na terytorium Rzeczypospolitej Polskiej z terytorium
+    państwa niebędącego państwem członkowskim:
+    1.  rzeczywistego ośrodka zarządzania spółki kapitałowej, jeżeli jej
+        siedziba nie znajduje się na terytorium państwa członkowskiego,
+    2.  siedziby spółki kapitałowej, jeżeli jej rzeczywisty ośrodek
+        zarządzania nie znajduje się na terytorium państwa
+        członkowskiego
+
+-- także wtedy, gdy czynność ta nie powoduje podwyższenia kapitału
+zakładowego.
+
+Opodatkowanie dotyczy umów, których przedmiotem są rzeczy znajdujące się
+na terytorium Polski i prawa majątkowe wykonywane na terytorium Polski.
+A jeśli są to rzeczy znajdujące się za granicą lub prawa majątkowe tam
+wykonywane -- umowa podlega PCC, jeśli ich nabywca mieszka albo ma
+siedzibę w Polsce i zawarł umowę w Polsce.
+
+W przypadku umowy zamiany wystarczy, że w Polsce jest jeden z
+zamienianych przedmiotów lub jedno z praw majątkowych jest wykonywane w
+Polsce.
+
+Podatek płacisz także, jeśli:
+
+-   zmienisz jedną z wymienionych umów i przez to podwyższy się podstawa
+    opodatkowania,
+-   orzeczenie sądu albo ugoda -- wywoła takie same skutki prawne, jak
+    powyższe czynności. Na przykład, jeśli sąd wyda orzeczenie, które
+    znosi współwłasność, trzeba zapłacić podatek, jak przy umowie o
+    zniesienie współwłasności.
+"""
+
+SD3_DESCRIPTION = """
+Podatkowi od spadków i darowizn podlega nabycie przez osoby fizyczne:
+
+- własności rzeczy, które znajdują się na terytorium Polski lub praw majątkowych wykonywanych na terytorium Polski, w drodze:
+- dziedziczenia, zapisu zwykłego, dalszego zapisu, zapisu windykacyjnego, polecenia testamentowego,
+- darowizny, polecenia darczyńcy,
+- zasiedzenia,
+- nieodpłatnego zniesienia współwłasności,
+- zachowku, jeżeli uprawniony nie uzyskał go w postaci uczynionej przez spadkodawcę darowizny lub w drodze dziedziczenia albo w postaci zapisu,
+- nieodpłatnej: renty, użytkowania oraz służebności.
+- praw do wkładu oszczędnościowego na podstawie dyspozycji wkładem na wypadek śmierci,
+- jednostek uczestnictwa na podstawie dyspozycji uczestnika funduszu inwestycyjnego otwartego albo specjalistycznego funduszu inwestycyjnego otwartego na wypadek jego śmierci,
+- własności rzeczy znajdujących się za granicą lub praw majątkowych wykonywanych za granicą, jeżeli w chwili otwarcia spadku lub zawarcia umowy darowizny nabywca był obywatelem polskim lub miał miejsce stałego pobytu na terytorium Polski.
+- Przepisy ustawy o nieodpłatnym zniesieniu współwłasności stosuje się odpowiednio do nieodpłatnego:
+- nabycia własności rzeczy wspólnej (wspólnego prawa majątkowego) albo jej części przez niektórych dotychczasowych współwłaścicieli na dalszą współwłasność;
+wyodrębnienia własności lokali na rzecz niektórych lub wszystkich współwłaścicieli
+"""
+
+DN1_DESCRIPTION = """
+Obowiązek podatkowy od nieruchomości powstaje z pierwszym dniem miesiąca następującego po miesiącu, w którym nastąpiło zdarzenie powodujące władanie nieruchomością lub jej składnikami (które wynika z prawa własności, samoistnego posiadania, wieczystego użytkowania czy posiadania zależnego). Jeżeli obowiązek podatkowy ma związek z istnieniem budowli albo budynku lub ich części, to powstaje 1 stycznia roku następującego po roku, w którym budowa została zakończona albo w którym rozpoczęto użytkowanie budowli albo budynku lub ich części przed ich ostatecznym wykończeniem.
+
+Obowiązek podatkowy wygasa z upływem miesiąca, w którym ustały okoliczności uzasadniające ten obowiązek.
+### Co jest opodatkowane
+- grunty,
+- budynki lub ich części,
+- budowle lub ich części związane z prowadzeniem działalności gospodarczej.
+
+### Kto płaci podatek
+- właściciele,
+- użytkownicy wieczyści,
+- samoistni posiadacze,
+"""
+
+DR1_DESCRIPTION = """
+Obowiązek podatkowy rolny powstaje z pierwszym dniem miesiąca następującego po miesiącu, w którym podatnik objął grunt: na własność, w samoistne lub zależne posiadanie, lub w wieczyste użytkowanie.
+Obowiązek podatkowy wygasa ostatniego dnia miesiąca, w którym przestały istnieć powyższe okoliczności.
+
+Podatek płacą:
+- właściciele,
+- posiadacze samoistni,
+- użytkownicy wieczyści,
+- posiadacze gruntów, które są własnością Skarbu Państwa lub jednostki samorządu terytorialnego, jeżeli posiadanie:
+- wynika z umowy zawartej z właścicielem, z Krajowym Ośrodkiem Wsparcia Rolnictwa lub z innego tytułu prawnego albo jest bez tytułu prawnego, z wyjątkiem gruntów, które wchodzą w skład Zasobu Własności Rolnej Skarbu Państwa lub są w zarządzie Lasów Państwowych.W tym przypadku podatnikami są odpowiednio jednostki organizacyjne Krajowego Ośrodka Wsparcia Rolnictwa i Lasów Państwowych.
+- dzierżawcy gruntów gospodarstwa rolnego, które zostały w całości lub części wydzierżawione na podstawie umowy zawartej stosownie do przepisów o ubezpieczeniu społecznym rolników lub przepisów dotyczących uzyskiwania rent strukturalnych.
+"""
