46ff966d9e3a6f1e95b9840949a6ea1afc2086dd
Mikolaj Szawerda <mszawerda@box.com>
2024-09-28 15:48:42 +0200

    add jaas

--- docs/parser.py
+++ docs/parser.py
@@ -3,7 +3,7 @@ import json
 import sys
 
 # Replace 'your_schema.xsd' with your actual XSD file path
-xsd_file = 'schema.xsd'
+xsd_file = 'schemas/KodyKrajow_v13-0E.xsd'
 
-with open("schema.json", 'w') as fh:
+with open("schemas/kraje_schema.json", 'w') as fh:
     json.dump(xmlschema.to_dict(xsd_file), fh, indent=4, ensure_ascii=False)
