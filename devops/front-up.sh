305966036362814d547741858b7a575a61d1b570
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-09-29 06:20:22 +0000

    fix: build gatsby

--- devops/front-up.sh
+++ devops/front-up.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /root/The-Hackstreet-Boys/frontend/ && node_modules/gatsby/cli.js serve -H 0.0.0.0 -p 9000
+cd /root/The-Hackstreet-Boys/frontend/ && node_modules/gatsby/cli.js build && node_modules/gatsby/cli.js serve -H 0.0.0.0 -p 9000
